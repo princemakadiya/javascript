95d12f1fb8101e10391c97cd37ef0d285377b078
princemakadiya <72595181+princemakadiya@users.noreply.github.com>
2022-03-31 17:56:20 +0530

    Update index.js
    watched the tutorial and practiced it simultaneously. completed the topics up to strings (6:40 in the tutorial).

--- index.js
+++ index.js
@@ -1,9 +1,195 @@
-var myName = 'prince makadiya';
-var myAge = 22;
+// console.log('Welcome, to complete JavaScript Course');
 
-console.log(myage);
+// var myName = 'prince';
+// var myAge = 22;
 
-var myName = "thapa";
+// console.log(myage);
+// console.log(myName);
 
-console.log(myName);
+// console.log( 10 + "20");
+// console.log( "Java "+ "Script");
+// console.log( " " + 0);
 
+// console.log("prince " - "makadiya");
+
+// console.log(5+20);
+
+// console.log(23+31);
+// console.log(105-5);
+// console.log(250/5);
+// console.log(25*6);
+// console.log(27%4);
+
+// var num = 155;
+// console.log(num);
+// var newNum = --num + 5; 
+// console.log(newNum);
+
+//console.log(!true);
+
+// console.log("Hello World");
+
+// console.log("hello " +  "world");
+
+// var myName = "prince";
+
+// console.log(myName + " prince");
+
+// console.log(9**2); 
+
+// console.log(5 + "abcd");
+
+
+// var num1 = 5;
+// var num2 = '5';
+
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+
+// console.log(num1 == num2 );
+
+// var num1 = 5;
+// var num2 = '5';
+
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log(num2);
+
+// console.log(num1 === num2 );
+
+// var tomr = 'sunny';
+
+// if(tomr == 'rain'){
+//   console.log('take a raincoat');
+// }else{
+//   console.log('No need to take a raincoat');
+// }
+
+// var year = 2020;
+// if(year % 4 === 0){
+//   if(year % 100 === 0){
+//     if(year % 400 === 0){
+//       console.log("The year " + year + " is a leap year");
+//     }else{
+//       console.log("The year " + year + " is not a leap year");
+//     }
+//   }else{
+//     console.log("The year " + year + " is a leap year");
+//   }
+// }else{
+//   console.log("The year " + year + " is not a leap year");
+// }
+
+// var age = 17;
+// if(age >= 18){
+//   console.log("you are eligible to vote");
+// }else{
+//   console.log("you are not eligible to vote");
+// }
+
+
+// switch(area){
+//   case 'circle': 
+//     console.log("the area of the circle is : " + PI*r**2);
+//     break;
+
+//   case 'triangle':
+//     console.log("the area of the triangle is : " + (l*b)/2);
+//     break;
+
+//   case 'rectangle':
+//     console.log("the area of the rectangle is : " + (l*b));
+//     break;
+
+//   default:
+//     console.log("please enter valid data");
+// }   
+
+// for(var num = 1; num<= 10; num++){
+//     var tableOf = 12;  
+//   console.log(tableOf + " * " + num + " = " + tableOf * num);
+// }
+
+
+// function sum(){
+//   var a = 10, b = 40;
+//   var total = a+b;
+//   console.log(total);
+// }
+
+// function sum(a,b){
+//   var total = a+b;
+//   console.log(total);
+// }
+
+// sum(20,30);
+
+
+
+// var myName = "prince";
+// console.log(myName);
+
+// let myName = "prince";
+// console.log(myName);
+
+// myName = "prince";
+// console.log(myName);
+
+
+// const myName = "prince";
+// console.log(myName);
+
+// myName = "prince";
+// console.log(myName);
+
+
+// const sum = () => `the sum of the two number is ${(a=5)+(b=6)}`;
+
+// console.log(sum()); 
+
+// const colors = ['red', 'green', 'blue', 'white', 'pink'];
+// const isPresent = colors.includes('purple');
+// console.log(isPresent);
+
+// var myFriends = ['a','b','c','d'];
+
+// console.log(myFriends.length);
+
+
+// for(let elements of myFriends){
+//   console.log(elements);
+// }
+
+// var myFriends = ['a','b','c','d'];
+
+// myFriends.forEach(function(element, index, array) {
+//       console.log(element + " index : " +
+//         index + " " + array);
+// });
+
+// myFriends.forEach((element, index, array) => {
+//   console.log(element + " index : " +
+//     index + " " + array);
+// });
+
+
+// var myFriendNames = ["a","b","c","d"];
+
+// console.log(myFriendNames.includes("e"));
+
+
+// const prices = [200,300,350,400,450,500,600];
+
+// const newPriceTag = prices.filter((elem, index) => {
+//   return elem > 400;
+// })
+// console.log(newPriceTag);
+
+// const months = ['March', 'Jan', 'Feb', 'April' , 'Dec', 'Nov'];
+
+// console.log(months.sort());
+
+// const array1 = [1, 30, 4, 21, 100000, 99];
+// console.log(array1.sort());
+
+//----------------------------- watched upto 6:40   ------------------------------------
