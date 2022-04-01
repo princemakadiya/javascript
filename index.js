a19fc48f6b8ee9964e6e7af6e03e4f7fe54a4d4b
princemakadiya <72595181+princemakadiya@users.noreply.github.com>
2022-04-01 21:30:27 +0530

    Update index.js
    watched the tutorial till 11:16

--- index.js
+++ index.js
@@ -192,4 +192,249 @@
 // const array1 = [1, 30, 4, 21, 100000, 99];
 // console.log(array1.sort());
 
-//----------------------------- watched upto 6:40   ------------------------------------
+
+
+
+
+
+
+
+// array.sort(function(a,b){
+//     console.log(a,b);
+//     if(a>b){
+//         return 1;
+//     }
+//     if(a<b){
+//         return -1;
+//     }
+//     if(a==b){
+//         return 0;
+//     }
+// });
+// console.log(array);
+
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let sortedFruits = fruits.sort();
+// // let reversedFruits = sortedFruits.reverse();
+
+// console.log(sortedFruits)
+// // console.log(reversedFruits)
+
+// const animals = ['pigs', 'goats', 'sheep'];
+
+// // const count = animals.push('chicken');
+// // console.log(count);
+
+// animals.push('chicken', 'cats','cow');
+// console.log(animals);
+
+
+// const animals = ['pigs', 'goats', 'sheep'];
+
+// const count = animals.unshift('chicken');
+// console.log(count);
+// console.log(animals);
+
+// animals.unshift('chicken', 'cats','cow');
+// console.log(animals);
+
+
+// const plants = ['onion', 'cucumber',  'kale', 'tomato', 'cabbage'];
+
+// console.log(plants);
+// console.log(plants.pop());
+// console.log(plants);
+// console.log(plants.shift());
+
+
+
+// const months = ['Jan', 'march', 'April', 'June', 'July'];
+// const newMonth = months.splice(months.length,0,"Dec");
+// console.log(months);
+
+
+// const indexOfMonth = months.indexOf('June');
+
+// if(indexOfMonth != -1){
+//   const updateMonth = months.splice(indexOfMonth,1,'june');
+//   console.log(months);
+// }else{
+//   console.log('No data found');
+// // }
+//  const array1 = [41, 44, 29, 1, 51];
+// let newArr = array1.map((curElem,index,arr) => {
+//     return curElem > 9;
+// })
+// console.log(array1);
+// console.log(newArr);
+
+
+// let array = [25, 36, 49, 64, 81];
+
+// let arrSqr = array.map((curElem) =>  Math.sqrt(curElem) )
+// console.log(arrSqr);
+
+
+// const arr = [
+//         ['a_1', 'a_2'],
+//         ['a_3', 'a_4'],
+//         ['a_5', 'a_6'],
+//         ['a_7', ['a_7', ['a_7', 'a_8']]]
+//     ];
+
+// let flatArr = arr.reduce((accum, currVal)  => { 
+//           return accum.concat(currVal);
+// })
+
+// console.log(arr.flat(Infinity));
+
+// console.log(flatArr);
+
+
+// let myName = "prince makadiya";
+
+// console.log(myName);
+// console.log(myName.length);
+
+
+// const myBioData =  'I am prince makadiya';
+// console.log(myBioData.lastIndexOf("p"));
+
+
+// var str = "Apple, Bananaa, Kiwi, mango";
+
+// // let res = str.slice(0,4);
+// let res = str.slice(7);
+// console.log(res);
+
+// var str = "Apple, Bananaa, Kiwi";
+// let res = str.substring(8,-2);
+// console.log(res);
+
+
+// let str = "prince makadiya";
+
+// console.log(str.charAt(9));
+
+// let myName = "michal jackson";
+// console.log(myName.toUpperCase()); 
+// console.log(myName.toLowerCase());
+
+// var str = "              Hello         World!            ";
+// console.log(str.trim());
+
+// let currDate = new Date(); 
+// console.log(currDate);
+
+// console.log(new Date());
+// console.log(new Date().toLocaleString()); 
+// console.log(new Date().toString()); 
+
+// console.log(Date.now());
+
+// const curDate = new Date();
+
+// console.log(curDate.toLocaleString());
+// console.log(curDate.getFullYear());
+// console.log(curDate.getMonth());
+// console.log(curDate.getDate());
+// console.log(curDate.getDay());
+
+// const curTime = new Date();
+
+// console.log(curTime.getTime());
+// console.log(curTime.getMinutes());
+// console.log(curTime.getSeconds());
+// console.log(curTime.getMilliseconds());
+
+
+
+
+
+// console.log(Math.PI);
+
+// let num = 10.501;
+// console.log(Math.round(num));
+
+
+// console.log(Math.pow(2,3));
+// console.log(2**3);
+
+
+// console.log(Math.sqrt(25));
+// console.log(Math.sqrt(81));
+// console.log(Math.sqrt(66));
+
+// console.log(Math.abs(-955));
+// console.log(Math.abs(4-6));
+
+// console.log(Math.round(69.1));
+
+// console.log(Math.floor(259.1)); 
+
+// console.log(Math.min(0, 150, 30, 20, -8, -200));
+
+
+// console.log(Math.max(0, 150, 30, 20, -8, -200));
+
+// console.log(Math.floor(Math.random()*10));
+
+
+// console.log(Math.trunc(-356.1));
+
+
+  // let bioData = { 
+  //     myName : "prince makadiya",
+  //     myAge : 22,
+  //     getData : function(){
+  //       console.log(`My name is ${bioData.myName} and my age is ${bioData.myAge}`);
+  //     }
+  // }
+
+  // bioData.getData();
+
+// let bioData = { 
+//   myName : "prince makadiya",
+//   myAge : 22,
+//   getData (){
+//     console.log(`My name is ${bioData.myName} and my age is ${bioData.myAge}`);
+//   }
+// }
+
+// bioData.getData();
+
+
+// let bioData = { 
+//   myName : {
+//     realName : "prince",
+//   },
+//   myAge : 22,
+//   getData (){
+//     console.log(`My name is ${bioData.myName} and my age is ${bioData.myAge}`);
+//   }
+// }
+
+// console.log(bioData.myName.realName );
+
+
+ 
+// console.log(this.alert('This is alert....'));
+
+
+// function myName() {
+//     console.log(this);
+// }
+
+
+// const obj = {
+//     myAge : 26,
+//     myName : () => {
+//       console.log(this);
+//     }
+// }
+// obj.myName();
+
+
+//------------------------------   watched till 11:16   -----------------
